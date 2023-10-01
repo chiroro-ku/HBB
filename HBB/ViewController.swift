64de1a40b1e5d4251842bd2ae457f1e9b6338465
chiroro-ku <chiroro1582@gmail.com>
2023-10-02 01:39:57 +0900

    Initial Commit

---
+++ HBB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HBB
+//
+//  Created by 小林千浩 on 2023/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
